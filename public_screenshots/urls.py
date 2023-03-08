4ec7bc1891cfd2a2653c5c58772481e9bf311f15
Damien <db.webapp@gmail.com>
2023-03-08 16:56:44 +0000

    Editing a screenshot only if user logged is the owner : done

--- public_screenshots/urls.py
+++ public_screenshots/urls.py
@@ -3,4 +3,5 @@ from public_screenshots import views
 
 urlpatterns = [
     path('public-scrshot/', views.PublicScreenshotList.as_view()),
+    path('public-scrshot/<int:pk>/', views.PublicScreenshotDetail.as_view()),
 ]
--- public_screenshots/views.py
+++ public_screenshots/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import PublicScreenshot
 from .serializers import PublicScreenshotSerializer
-
+from scrshot_api.permissions import IsOwnerOrReadOnly
 
 class PublicScreenshotList(APIView):
     serializer_class = PublicScreenshotSerializer
@@ -33,3 +33,63 @@ class PublicScreenshotList(APIView):
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
 
+
+
+
+class PublicScreenshotDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = PublicScreenshotSerializer
+
+    def get_object(self, pk):
+        try:
+            public_screenshot = PublicScreenshot.objects.get(pk=pk)
+            self.check_object_permissions(self.request, public_screenshot)
+            return public_screenshot
+        except PublicScreenshot.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        public_screenshot = self.get_object(pk)
+        serializer = PublicScreenshotSerializer(
+            public_screenshot, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        public_screenshot = self.get_object(pk)
+        serializer = PublicScreenshotSerializer(
+            public_screenshot, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        public_screenshot = self.get_object(pk)
+        public_screenshot.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+
+
+
+
+
+
+
+
+
+        
+    def put(self, request, pk):
+        PublicScreenshot = self.get_object(pk)
+        serializer = PublicScreenshotSerializer(
+            PublicScreenshot, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
