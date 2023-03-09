53f15335d42f4f8f4096a6ebaf56b86538059384
Damien <db.webapp@gmail.com>
2023-03-09 15:44:11 +0000

    public_screenshot app refactoring views with generics

--- public_screenshots/serializers.py
+++ public_screenshots/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from .models import PublicScreenshot
-
+from likes.models import Like
 
 class PublicScreenshotSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -26,6 +29,15 @@ class PublicScreenshotSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, public_screenshot=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = PublicScreenshot
         fields = [
@@ -39,4 +51,7 @@ class PublicScreenshotSerializer(serializers.ModelSerializer):
             'is_owner',
             'profile_id',
             'profile_image',
+            'like_id', 
+            'likes_count', 
+            'comments_count',
         ]
--- public_screenshots/views.py
+++ public_screenshots/views.py
@@ -1,95 +1,56 @@
 from django.http import Http404
 from rest_framework import status
-from rest_framework import permissions
+from rest_framework import generics, permissions, filters
 from rest_framework.views import APIView
+from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from .models import PublicScreenshot
 from .serializers import PublicScreenshotSerializer
 from scrshot_api.permissions import IsOwnerOrReadOnly
 
-class PublicScreenshotList(APIView):
+class PublicScreenshotList(generics.ListCreateAPIView):
     serializer_class = PublicScreenshotSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
 
-    def get(self, request):
-        PublicScreenshots = PublicScreenshot.objects.all()
-        serializer = PublicScreenshotSerializer(
-            PublicScreenshots, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    queryset = PublicScreenshot.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
+    ]
 
-    def post(self, request):
-        serializer = PublicScreenshotSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 
 
-class PublicScreenshotDetail(APIView):
+class PublicScreenshotDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = PublicScreenshotSerializer
-
-    def get_object(self, pk):
-        try:
-            public_screenshot = PublicScreenshot.objects.get(pk=pk)
-            self.check_object_permissions(self.request, public_screenshot)
-            return public_screenshot
-        except PublicScreenshot.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        public_screenshot = self.get_object(pk)
-        serializer = PublicScreenshotSerializer(
-            public_screenshot, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        public_screenshot = self.get_object(pk)
-        serializer = PublicScreenshotSerializer(
-            public_screenshot, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        public_screenshot = self.get_object(pk)
-        public_screenshot.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = PublicScreenshot.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
 
 
-
-
-
-
-
-
-
-
-        
-    def put(self, request, pk):
-        PublicScreenshot = self.get_object(pk)
-        serializer = PublicScreenshotSerializer(
-            PublicScreenshot, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
