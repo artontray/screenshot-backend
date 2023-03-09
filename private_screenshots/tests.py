cbfd3a58b65c2414dd2609d3e05c82aa1e13b186
Damien <db.webapp@gmail.com>
2023-03-09 22:56:41 +0000

    Making tests on private/public_screenshots app : tests.py

--- private_screenshots/tests.py
+++ private_screenshots/tests.py
@@ -1,3 +1,75 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import PrivateScreenshot
+from category.models import Category
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PrivateScreenshotDetailViewTests(APITestCase):
+    """
+    PrivateScreenshot Detail View Testings
+    """
+    def setUp(self):
+        me = User.objects.create_user(username='me', password='me@password')
+        category_default1 = Category.objects.create(
+            owner=me,
+            title='Main',
+            
+        )
+        not_me = User.objects.create_user(username='not_me', password='not_me@password')
+        category_default2 = Category.objects.create(
+            owner=not_me,
+            title='Main',
+            
+        )
+        PrivateScreenshot.objects.create(
+            owner=me, 
+            title='my first screenshot private', 
+            content='This is my very first screenshot', 
+            id=1,
+            category=category_default1,
+        )
+        
+        PrivateScreenshot.objects.create(
+            owner=not_me, 
+            title='my first screenshot private', 
+            content='This is my very first screenshot', 
+            id=2,
+            category=category_default2,
+        )
+
+
+    def test_exists_category_when_creating_user(self):
+        """
+        Testing if a category instance is created when created a User instance
+        """
+        self.client.login(username='me', password='me@password')
+        response = self.client.get('/category/1/')
+        self.assertEqual(response.data['title'], 'Main')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_read_category_when_not_logged_user(self):
+        """
+        Testing to read a category from an other user
+        """
+        response = self.client.get('/category/1/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    
+    def test_read_private_screenshot_not_own_by_user(self):
+        """
+        Testing to read a private screenshot from an other user
+        """
+        self.client.login(username='me', password='me@password')
+        response = self.client.get('/private-scrshot/2/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_read_private_screenshot_own_by_user(self):
+        """
+        Testing to read a private own screenshot 
+        """
+        self.client.login(username='not_me', password='not_me@password')
+        response = self.client.get('/private-scrshot/2/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- public_screenshots/tests.py
+++ public_screenshots/tests.py
@@ -1,3 +1,69 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import PublicScreenshot
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PublicScreenshotDetailViewTests(APITestCase):
+    """
+    PublicScreenshot Detail View Testings
+    """
+    def setUp(self):
+        me = User.objects.create_user(username='me', password='me@password')
+        not_me = User.objects.create_user(username='not_me', password='not_me@password')
+        PublicScreenshot.objects.create(
+            owner=me, title='my first screenshot', content='This is my very first screenshot', id=1
+        )
+        PublicScreenshot.objects.create(
+            owner=not_me, title='my first screenshot', content='This is my very first screenshot', id=2
+        )
+
+
+    def test_read_screenshot(self):
+        """
+        Testing a user reading a screenshot
+        """
+        response = self.client.get('/public-scrshot/1/')
+        self.assertEqual(response.data['title'], 'my first screenshot')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_read_screenshot_with_invalid_id(self):
+        """
+        Testing a user reading a screenshot using an invalid ID
+        """
+        response = self.client.get('/public-scrshot/99999999/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_delete_own_screenshot(self):
+        """
+        Testing a user deleting a own screenshot 
+        """
+        self.client.login(username='me', password='me@password')
+        response = self.client.delete('/public-scrshot/1/', format='json')
+        response = self.client.get('/public-scrshot/1/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_update_own_screenshot(self):
+        """
+        Testing a user updating a own screenshot 
+        """
+        self.client.login(username='me', password='me@password')
+        response = self.client.put('/public-scrshot/1/', {
+          'owner': 'me', 'title': 'new title'
+        }, format='json')
+        publicscreenshot =  PublicScreenshot.objects.filter(pk=1).first()
+        self.assertEqual(publicscreenshot.title, 'new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_update_not_own_screenshot(self):
+        """
+        Testing a user updating a screenshot from other user
+        """
+        self.client.login(username='me', password='me@password')
+        response = self.client.put('/public-scrshot/2/', {
+            'title': 'a new title'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
