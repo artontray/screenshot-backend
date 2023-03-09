ebbdaeeb944a06907326e50629265b8332604903
Damien <db.webapp@gmail.com>
2023-03-09 16:15:38 +0000

    Refactoring Profiles and added new fields to show on JSON

--- profiles/views.py
+++ profiles/views.py
@@ -2,19 +2,45 @@ from django.http import Http404
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.views import APIView
+from django.db.models import Count
+from rest_framework import generics, filters
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 from scrshot_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
     """
     List all profiles
-    No Create view (post method), as profile creation handled by django signals
+    No Create view here, profile creation handled by django signals
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        public_screenshots_count=Count('owner__publicscreenshot', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+    ]
+    ordering_fields = [
+        'public_screenshots_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+        
+    ]
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     '''
@@ -22,4 +48,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     '''
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        public_screenshots_count=Count('owner__publicscreenshot', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
