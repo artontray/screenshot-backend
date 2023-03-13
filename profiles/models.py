eee3df9127457a5afa43379dbf907759b1c75ac8
Damien <db.webapp@gmail.com>
2023-03-13 15:35:42 +0000

    Avatar default image changed

--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     description = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../logo_user_undefined_gom6nt'
+        upload_to='images/', default='../avatar_yidcjz'
     )
 
     class Meta:
