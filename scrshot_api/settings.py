5cfeaf6ca9a7ce569b2b8b6d3d42c7aa4811a2fe
Damien <db.webapp@gmail.com>
2023-03-15 22:02:07 +0000

    Deployment Heroku - still was errors during deployment, now fixed

--- scrshot_api/settings.py
+++ scrshot_api/settings.py
@@ -57,14 +57,16 @@ ALLOWED_HOSTS = [
 ]
 
 
+
 DATABASES = {
     'default': ({
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   } if 'DEV' in os.environ else dj_database_url.parse(
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    } if 'DEV' in os.environ else dj_database_url.parse(
         os.environ.get('DATABASE_URL')
-   ))
+    ))
 }
+
 
 #DATABASES = {
 #    'default': {
